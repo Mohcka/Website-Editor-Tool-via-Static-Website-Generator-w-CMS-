2d180c6bc0d94f3a4591eacd892931b5e51323c7
Mohcka <bandagedwolf95@gmail.com>
2019-09-23 12:39:18 -0700

    updated contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -12,6 +12,8 @@ const ContactForm = props => (
         </Title>
       </div>
       <form name="contact" method="POST" data-netlify="true">
+        {/* This is needed for gatsby to let netlify process the form submission */}
+        <input type="hidden" name="form-name" value="contact" />
         <div className="form-group">
           <label for="nameField">Your Name:</label>
           <input type="text" name="name" class="form-control" id="nameField" />
@@ -27,12 +29,7 @@ const ContactForm = props => (
         </div>
         <div className="form-group">
           <label for="phoneField">Your Phone Number:</label>
-          <input
-            type="tel"
-            name="phone"
-            class="form-control"
-            id="phoneField"
-          />
+          <input type="tel" name="phone" class="form-control" id="phoneField" />
         </div>
         <div className="form-group">
           <label for="message">Message:</label>
