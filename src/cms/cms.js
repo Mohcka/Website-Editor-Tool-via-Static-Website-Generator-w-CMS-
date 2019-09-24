6b400c333aa44017e6ae35af955ac0284138384a
Mohcka <bandagedwolf95@gmail.com>
2019-09-24 16:05:10 -0700

    custom cms widget test

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,4 @@
 import CMS from "gatsby-plugin-netlify-cms"
 import * as ColorWidget from "netlify-cms-widget-color"
 
-CMS.registerWidget("color", ColorWidget.Control)
+CMS.registerWidget("color", "text")
