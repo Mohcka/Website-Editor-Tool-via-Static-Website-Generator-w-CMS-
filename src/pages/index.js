348c518f97ff32e6ec32115049e85e3245118e9a
Mohcka <bandagedwolf95@gmail.com>
2019-09-24 09:54:29 -0700

    cleaned up index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,4 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
-import GImg from "gatsby-image"
-// import GImgIE
-
-import {
-  Container,
-  Row,
-  Col,
-  Image as BSImage,
-  Card,
-  Accordion,
-  Button,
-} from "react-bootstrap"
-
-import styled from "styled-components"
-import FlakeTheme from "../components/styles/FlakeTheme"
-import varStyles, { Title } from "../components/styles/FlakeTheme"
-
-import { useAccordionToggle } from "react-bootstrap/AccordionToggle"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -35,6 +16,7 @@ const IndexPage = () => {
       <SEO title="Home" />
       <Sections />
       <ContactForm />
+      {/* TODO: Footer */}
     </Layout>
   )
 }
