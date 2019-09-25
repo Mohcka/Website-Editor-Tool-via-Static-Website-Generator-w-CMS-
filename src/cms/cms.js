f57738e14832070a4f6c33a6dd473143c0ab8663
Mohcka <bandagedwolf95@gmail.com>
2019-09-24 19:43:19 -0700

    update cms-app

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,32 @@
 import CMS from "netlify-cms-app"
-import React from "react"
+import React, { Component } from "react"
+import PropTypes from "prop-types"
 
-const Test = props => <input type="text" defaultValue={`${props.vlaue}`} />
+class Test extends Component {
+  static propTypes = {
+    onChange: PropTypes.func.isRequired,
+    forID: PropTypes.string,
+    value: PropTypes.node,
+    classNameWrapper: PropTypes.string.isRequired,
+  }
+
+  static defaultProps = {
+    value: "",
+  }
+
+  render() {
+    const { forID, value, onChange, classNameWrapper } = this.props
+
+    return (
+      <input
+        type="text"
+        id={forID}
+        defaultValue={value || ""}
+        className={classNameWrapper}
+        onChange={e => onChange(e.target.value)}
+      />
+    )
+  }
+}
 
 CMS.registerWidget("test", Test)
