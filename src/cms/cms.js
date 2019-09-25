b820f041571e59ea387c29aeb7083d4d612289f9
Mohcka <bandagedwolf95@gmail.com>
2019-09-24 17:23:01 -0700

    updated cms-app

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,4 +2,4 @@ import CMS from "netlify-cms-app"
 import "react"
 import * as ColorWidget from "netlify-cms-widget-color"
 
-CMS.registerWidget("color", "text")
+CMS.registerWidget("color", ColorWidget.Control)
