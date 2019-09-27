112837953a98a91d65d1912f06cabdc9f1879a90
Mohcka <bandagedwolf95@gmail.com>
2019-09-27 13:10:54 -0700

    updated promotions for cms-app

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -7,7 +7,7 @@ import PropTypes from "prop-types"
 import { SketchPicker } from "react-color"
 
 CMS.registerWidget(
-  "fontawesome",
+  "fontawesome-solid",
   FontawesomeWidget.Solid,
   FontawesomeWidget.Preview
 );
