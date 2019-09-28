fceca5bbfa15d80f9f7388541af4dc8d425561d7
Mohcka <bandagedwolf95@gmail.com>
2019-09-28 13:22:20 -0700

    updated gallery

---
+++ src/components/Sections/MasonryGallery.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react"
+import Loadable from "react-loadable"
+import PropTypes from "prop-types"
+
+import styled from "styled-components"
+import { Title } from "../styles/FlakeTheme"
+import Macy from "macy"
+
+const StyledTest = styled.div`
+  background: red
+`
+
+const PopoutImage = props => {
+  const triggerPopout
+
+
+}
+
+const Gallery = props => {
+  useEffect(() => {
+    Macy({
+      container: "#masonry-gallery",
+    })
+  })
+
+  return (
+    <>
+      <Title>Gallery</Title>
+      <div id="masonry-gallery">
+        {props.gallery_imgs.map((image, i) => (
+          <div key={i} style={{ cursor: "pointer" }} data-gallery-image onClick={e => console.log(e.target)}>
+            {image}
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+Gallery.propTypes = {
+  // The img elements for the gallery
+  gallery_imgs: PropTypes.arrayOf(PropTypes.element).isRequired,
+}
+
+export default Gallery
